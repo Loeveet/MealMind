f9cda53951389dd411a379f17b4742ec74fedfd9
Robin <loeveet@gmail.com>
2024-05-14 15:28:16 +0200

    Create testmethod for running scraper via workflow

---
+++ MasterMealMind/Helpers/Workflow.cs
@@ -0,0 +1,14 @@
+﻿using MasterMealMind.Core.Interfaces;
+
+namespace MasterMealMind.Web.Helpers
+{
+	public class Workflow(IGetIcaRecipies getIcaRecipies)
+	{
+		private readonly IGetIcaRecipies _getIcaRecipies = getIcaRecipies;
+
+		public async void RunScraper()
+		{
+			await _getIcaRecipies.GetIcaAsync();
+		}
+	}
+}
